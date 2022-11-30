1ef64fdd934d0996bf11501c02fb2eba47e062f7
Denis <inquis87@gmail.com>
2022-11-30 23:23:43 +0300

    1

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@
 // два числа и выдает, какое число большее, а какое меньшее.
 
 Console.WriteLine("Введите первое число: ");
- int num1 = int.Parse(Console.ReadLine());
- Console.WriteLine("Введите второе число: ");
- int num2 = int.Parse(Console.ReadLine());
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
  
  if(num1 > num2)
  {
